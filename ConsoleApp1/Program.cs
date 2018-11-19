2839e24aeac872fce0917572c2ab5ae8c1e902ef
HOME-PC\Антон <tony.baton@yandex.by>
2018-11-19 23:30:29 +0300

    full app

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ftp ftpClient = new ftp(@"ftp://192.168.0.100/", "user-ftp", "1234");
+            ftpClient.download("first/pass.txt", @"D:\THERE\new.txt");
+        }
+    }
+}
